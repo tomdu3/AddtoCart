142d7248b62fbedfaf79e664773303d2798cb959
Tomislav Dukez <tomdu3@ymail.com>
2025-02-17 11:04:48 +0000

    add Footer

--- add2cart-app/src/App.jsx
+++ add2cart-app/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 // Main App Component
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
       <Navbar />
+      <Footer />
     </div>
   );
 };
