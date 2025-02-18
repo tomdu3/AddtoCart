19295f614d14bef9bcb537409020e5888ac0503c
Tomislav Dukez <tomdu3@ymail.com>
2025-02-18 12:16:26 +0000

    fix update App to contain Cart functionalities

--- add2cart-app/src/App.jsx
+++ add2cart-app/src/App.jsx
@@ -1,185 +1,67 @@
-import React from 'react';
+
+import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import ProductCard from './components/ProductCard';
 import ProductsTable from './components/ProductsTable';
+import CartModal from './components/CartModal';
+import Alert from './components/Alert';
+import { products } from './data/products';
+import Hero from './components/Hero';
 import Footer from './components/Footer';
 
-// Main App Component
 const App = () => {
-  // make starting point data
-  const products = [
-  {
-    id: 1,
-    title: "Fjallraven - Foldsack No. 1 Backpack",
-    price: 109.95,
-    category: "men's clothing",
-    description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-  },
-  {
-    id: 2, 
-    title: "Mens Casual Premium Slim Fit T-Shirts",
-    price: 22.3,
-    category: "men's clothing",
-    description: "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, lightweight & soft fabric",
-    image: "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
-  },
-  {
-    id: 3,
-    title: "Mens Cotton Jacket",
-    price: 55.99,
-    category: "men's clothing", 
-    description: "Great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions",
-    image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"
-  },
-  {
-    id: 4,
-    title: "Mens Casual Slim Fit",
-    price: 15.99,
-    category: "men's clothing",
-    description: "The color could be slightly different between on the screen and in practice.",
-    image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg"
-  },
-  {
-    id: 5,
-    title: "John Hardy Women's Chain Bracelet",
-    price: 695,
-    category: "jewelery",
-    description: "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl.",
-    image: "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg"
-  },
-  {
-    id: 6,
-    title: "Solid Gold Petite Micropave",
-    price: 168,
-    category: "jewelery",
-    description: "Satisfaction Guaranteed. Return or exchange any order within 30 days.",
-    image: "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg"
-  },
-  {
-    id: 7,
-    title: "White Gold Plated Princess",
-    price: 9.99,
-    category: "jewelery",
-    description: "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her.",
-    image: "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg"
-  },
-  {
-    id: 8,
-    title: "Pierced Owl Rose Gold Plated",
-    price: 10.99,
-    category: "jewelery",
-    description: "Rose Gold Plated Double Flared Tunnel Plug Earrings.",
-    image: "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg"
-  },
-  {
-    id: 9,
-    title: "WD 2TB External Hard Drive",
-    price: 64,
-    category: "electronics",
-    description: "USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity",
-    image: "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg"
-  },
-  {
-    id: 10,
-    title: "SanDisk SSD PLUS 1TB",
-    price: 109,
-    category: "electronics",
-    description: "Easy upgrade for faster boot up, shutdown, application load and response",
-    image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg"
-  },
-  {
-    id: 11,
-    title: "Silicon Power 256GB SSD",
-    price: 109,
-    category: "electronics",
-    description: "3D NAND flash are applied to deliver high transfer speeds",
-    image: "https://fakestoreapi.com/img/71kWymZ+c+L._AC_SX679_.jpg"
-  },
-  {
-    id: 12,
-    title: "WD 4TB Gaming Drive",
-    price: 114,
-    category: "electronics",
-    description: "Expand your PS4 gaming experience, Play anywhere",
-    image: "https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_.jpg"
-  },
-  {
-    id: 13,
-    title: "Acer SB220Q bi 21.5 inches",
-    price: 599,
-    category: "electronics",
-    description: "21.5 inches Full HD (1920 x 1080) widescreen IPS display",
-    image: "https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg"
-  },
-  {
-    id: 14,
-    title: "Samsung 49-Inch CHG90",
-    price: 999.99,
-    category: "electronics",
-    description: "49 INCH SUPER ULTRAWIDE 32:9 CURVED GAMING MONITOR",
-    image: "https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_.jpg"
-  },
-  {
-    id: 15,
-    title: "BIYLACLESEN Women's Snowboard Jacket",
-    price: 56.99,
-    category: "women's clothing",
-    description: "Note:The Jackets is US standard size, Please choose size as your usual wear",
-    image: "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg"
-  },
-  {
-    id: 16,
-    title: "Lock and Love Women's Jacket",
-    price: 29.95,
-    category: "women's clothing",
-    description: "100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER)",
-    image: "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg"
-  },
-  {
-    id: 17,
-    title: "Rain Jacket Women Windbreaker",
-    price: 39.99,
-    category: "women's clothing",
-    description: "Lightweight perfet for trip or casual wear---Long sleeve with hooded",
-    image: "https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg"
-  },
-  {
-    id: 18,
-    title: "MBJ Women's Solid Short Sleeve",
-    price: 9.85,
-    category: "women's clothing",
-    description: "95% RAYON 5% SPANDEX, Made in USA or Imported",
-    image: "https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg"
-  },
-  {
-    id: 19,
-    title: "Opna Women's Short Sleeve",
-    price: 7.95,
-    category: "women's clothing",
-    description: "100% Polyester, Machine wash, 100% cationic polyester interlock",
-    image: "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg"
-  },
-  {
-    id: 20,
-    title: "DANVOUY Women's T Shirt",
-    price: 12.99,
-    category: "women's clothing",
-    description: "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print",
-    image: "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg"
-  }
-]; 
+    const [cartItems, setCartItems] = useState([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [alert, setAlert] = useState({ message: '', type: '', visible: false });
 
-return (
-    <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
-      <Navbar />
-      <Hero />
-      {/* <ProductCard product={products[0]} /> */}
-      <ProductsTable products={products} />
-      <Footer />
-    </div>
-  );
+    useEffect(() => {
+        localStorage.setItem('cartItems', JSON.stringify(cartItems));
+    }, [cartItems]);
+
+    const showAlert = (message, type) => {
+        setAlert({ message, type, visible: true });
+        setTimeout(() => {
+            setAlert({ message: '', type: '', visible: false });
+        }, 3000);
+    };
+
+    const handleAddToCart = (product) => {
+        const existingItem = cartItems.find((item) => item.id === product.id);
+        if (existingItem) {
+            showAlert('Item already added to cart', 'error');
+        } else {
+            setCartItems([...cartItems, { ...product, quantity: 1 }]);
+            showAlert('Item added to cart successfully', 'success');
+        }
+    };
+
+    const handleRemoveFromCart = (productId) => {
+        setCartItems(cartItems.filter((item) => item.id !== productId));
+        showAlert('Item removed from cart', 'success');
+    };
+
+    const handleOpenModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    };
+
+    return (
+        <div>
+            <Navbar cartItemCount={cartItems.length} handleOpenModal={handleOpenModal} />
+            <Hero />
+            <ProductsTable products={products} handleAddToCart={handleAddToCart} />
+            <CartModal
+                cartItems={cartItems}
+                handleRemoveFromCart={handleRemoveFromCart}
+                handleCloseModal={handleCloseModal}
+                isModalOpen={isModalOpen}
+            />
+            <Footer />
+            {alert.visible && <Alert message={alert.message} type={alert.type} />}
+        </div>
+    );
 };
 
 export default App;
