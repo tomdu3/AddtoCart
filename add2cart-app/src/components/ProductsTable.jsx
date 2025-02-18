df44691c3dda182e09f40877ed9027718b8f418c
Tomislav Dukez <tomdu3@ymail.com>
2025-02-18 12:14:13 +0000

    fix update Products Table

--- add2cart-app/src/components/ProductsTable.jsx
+++ add2cart-app/src/components/ProductsTable.jsx
@@ -1,20 +1,21 @@
+import React from 'react';
 import ProductCard from "./ProductCard";
 
-const ProductsTable = ({ products }) => {
+const ProductsTable = ({ products, handleAddToCart }) => {
     return (
-      <section className="py-20 px-6">
-        <div className="container mx-auto">
-          <h2 className="text-3xl font-bold text-gray-900 dark:text-white mb-12">
-            Our Products
-          </h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-            {products.map(product => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </div>
-      </section>
+        <section className="py-20 px-6">
+            <div className="container mx-auto">
+                <h2 className="text-3xl font-bold text-gray-900 dark:text-white mb-12">
+                    Our Products
+                </h2>
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+                    {products.map(product => (
+                        <ProductCard key={product.id} product={product} handleAddToCart={handleAddToCart} />
+                    ))}
+                </div>
+            </div>
+        </section>
     );
-  };
+};
 
-  export default ProductsTable;
+export default ProductsTable;
